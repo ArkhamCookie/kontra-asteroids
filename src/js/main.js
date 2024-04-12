8d0f3f7e1e53263d772c2b8b882550696b5bc56d
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-12 15:11:02 -0500

    Feat: Make creating an asteroid a function

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
 import { kontra } from '../lib/kontra.mjs'
 
 const { canvas, context } = kontra.init()
+let sprites = []
 
-const asteroid = kontra.Sprite({
-	type: 'asteroid',
-	x: 100,
-	y: 100,
-	dx: Math.random() * 4 - 2,
-	dy: Math.random() * 4 - 2,
-	radius: 30,
-	render() {
-		this.context.strokeStyle = 'white'
-		this.context.beginPath()
-		this.context.arc(0, 0, this.radius, 0, Math.PI*2)
-		this.context.stroke()
-	}
-})
-asteroid.render()
+function createAsteroid() {
+	const asteroid = kontra.Sprite({
+		type: 'asteroid',
+		x: 100,
+		y: 100,
+		dx: Math.random() * 4 - 2,
+		dy: Math.random() * 4 - 2,
+		radius: 30,
+		render() {
+			this.context.strokeStyle = 'white'
+			this.context.beginPath()
+			this.context.arc(0, 0, this.radius, 0, Math.PI*2)
+			this.context.stroke()
+		}
+	})
+	sprites.push(asteroid)
+}
 
 const loop = kontra.GameLoop({
 	update() {
